3371b67b934605fc7edf88439be8c5f9ea93b38f
AzuriteLab <azurite.cg@gmail.com>
2023-08-09 00:19:45 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+﻿現状は特定のチャンネルにVRChatのユーザページを含むメッセージを投稿したら指定したロールを外すだけのDiscord BOT
+
+実装はDiscord API v10ベースです
+
+## Require
+
+app_settings.jsonがindex.jsと同じ階層に必要です
+
+```json
+{
+    "discord_token":"<ディスコードのトークン>",
+    "target_role_id":"<外したいロール>",
+    "target_channel_id":"<監視したいチャンネルのID>"
+}
+```
---
+++ index.js
@@ -0,0 +1,48 @@
+﻿const async = require('async');
+const util = require('util');
+const {Client, GatewayIntentBits} = require('discord.js');
+
+const fs = require('fs');
+
+const target_channel_id = "1138104307489714348";
+const target_role_id = "1138468318764875917";
+
+const discord_client = new Client({intents: 
+    [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ]
+});
+if (!fs.existsSync("./app_settings.json", "utf8")) {
+    const default_app_settings_structure = {
+        "discord_token": "",
+        "target_role_id":"",
+        "target_channel_id":""
+    };
+    fs.writeFileSync("./app_settings.json", JSON.stringify(default_app_settings_structure));
+    console.log("need to edit app_settings.json and add the token.");
+    return;
+}
+const app_settings = JSON.parse(fs.readFileSync("./app_settings.json", "utf8"));
+
+discord_client.on('ready', async () => {
+    console.log(`Logged in as ${discord_client.user.tag}!`);
+});
+
+discord_client.on('messageCreate', async (msg) => {
+    
+    if (msg.author.bot || msg.channel.id != target_channel_id) {
+        return;
+    }
+    if (!msg.content.includes('https://vrchat.com/home/user/usr_')) {
+        return;
+    }
+    if (msg.member.roles.cache.has(app_settings.target_role_id)) {
+        msg.member.roles.remove(app_settings.target_role_id);
+        console.log(`[${msg.createdAt.toLocaleString()}] User's role update: ${msg.author.globalName}`);
+    }
+});
+
+discord_client.login(app_settings.discord_token);
